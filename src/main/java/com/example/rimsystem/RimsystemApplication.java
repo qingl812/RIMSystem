2d76fde31e364a65e457a56c1d57f101e6dba7e5
QINGL-REDMI\qingl <412753863@qq.com>
2021-09-06 10:07:23 +0800

    创建项目

---
+++ src/main/java/com/example/rimsystem/RimsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.rimsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RimsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RimsystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/rimsystem/controller/RoadController.java
@@ -0,0 +1,16 @@
+package com.example.rimsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @auther luyu
+ */
+@Controller
+@RequestMapping("/road")
+public class RoadController {
+    @RequestMapping("/dosth")
+    public String dosth(){
+        return "hello";
+    }
+}
