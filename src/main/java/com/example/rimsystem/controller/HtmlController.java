2a77f57996c8c975f1946760aebf04556c82fc81
QINGL-REDMI\qingl <412753863@qq.com>
2021-09-14 17:40:59 +0800

    小修改

--- src/main/java/com/example/rimsystem/controller/HtmlController.java
+++ src/main/java/com/example/rimsystem/controller/HtmlController.java
@@ -12,7 +12,87 @@ import java.util.List;
 @Controller
 public class HtmlController {
     @RequestMapping("/homePage")
-    public String doHomePage() {
+    public String homePage() {
         return "homePage";
+    }
+
+    @RequestMapping("/login")
+    public String logIn() {
+        return "login";
+    }
+
+    @RequestMapping("/maintenanceManagement")
+    public String maintenanceManagement() {
+        return "maintenanceManagement";
+    }
+
+    @RequestMapping("/moneyManagement")
+    public String moneyManagement() {
+        return "moneyManagement";
+    }
+
+    @RequestMapping("/roadDetection")
+    public String roadDetection() {
+        return "roadDetection";
+    }
+
+    @RequestMapping("/roadInformation")
+    public String roadInformation() {
+        return "roadInformation";
+    }
+
+    @RequestMapping("/systemManagement")
+    public String systemManagement() {
+        return "systemManagement";
+    }
+
+    @RequestMapping("/visaRecord")
+    public String visaRecord() {
+        return "visaRecord";
+    }
+
+    @RequestMapping("/workCommunication")
+    public String workCommunication() {
+        return "workCommunication";
+    }
+
+    @RequestMapping("/sharedPages/head")
+    public String head() {
+        return "sharedPages/head";
+    }
+
+    @RequestMapping("/sharedPages/navigation")
+    public String navigation() {
+        return "sharedPages/navigation";
+    }
+
+    @RequestMapping("/sharedPages/status")
+    public String status() {
+        return "sharedPages/status";
+    }
+
+    @RequestMapping("/frames/map")
+    public String map() {
+        return "frames/map";
+    }
+
+    @RequestMapping("/frames/mapStatus")
+    public String mapStatus() {
+        return "frames/mapStatus";
+    }
+
+    @RequestMapping("/frames/roadInfoSelect")
+    public String roadInfoSelect() {
+        return "frames/roadInfoSelect";
+    }
+
+    @RequestMapping("/frames/roadInfoWrite")
+    public String roadInfoWrite() {
+        return "frames/roadInfoWrite";
+    }
+
+    @RequestMapping("/frames/search")
+    public String search() {
+        return "frames/search";
     }
 }
